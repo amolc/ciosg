a29b63e7601273faa769333c495556aa241abda1
lomteankush2 <lomteankush2@gmail.com>
2015-03-09 13:46:53 +0530

    for pdf format

--- generate_pdf.php
+++ generate_pdf.php
@@ -66,11 +66,12 @@ h1 { margin-bottom: 0; }z
 
 <div><label><strong>1) CONTACT INFORMATION</strong></label></div>
 <br />
-<table border="1" width="100%"><tr><td>First Name</td><td>'.$firstname.'</td></tr><tr><td>Last Name</td><td>'.$lastname.'</td></tr><tr><td>Designation</td><td>'.$designations.'</td></tr><tr><td>Email</td><td>'.$emailid.'</td></tr><tr><td>Mobile Phone</td><td>'.$mobile.'</td></tr></table>
+<table border="1" width="100%"><tr><td>First Name</td><td>'.$firstname.'</td></tr><tr><td>Last Name</td><td>'.$lastname.'</td></tr><tr><td>Designation</td><td>'.$designations.'</td></tr><tr><td>Email</td><td>'.$emailid.'</td></tr><tr><td>Telephone No</td><td>'.$mobile.'</td></tr><tr><td>Mobile Phone</td><td>'.$mobile.'</td></tr></table>
 <br /><br />
 <div><label><strong>2) COMPANY INFORMATION</strong></label></div>
 <br />
-<table border="1" width="100%"><tr><td>Company Name</td><td>'.$cname.'</td></tr><tr><td height="50">Company Address</td><td>'.$add1.'</td></tr><tr><td>City</td><td>'.$city.'</td></tr><tr><td>Country</td><td>'.$country.'</td></tr><tr><td>Telephone No</td><td>'.$phone_number.'</td></tr></table>
+<table border="1" width="100%"><tr><td>Company Name</td><td>'.$cname.'</td></tr><tr><td height="50">Company Address</td><td>'.$add1.'</td></tr><tr><td>City</td><td>'.$city.'</td></tr><tr><td>State</td><td>'.$city.'</td></tr><tr><td>Postal Code/ ZIP</td><td>'.$city.'</td></tr><tr><td>Country</td><td>'.$country.'</td></tr><tr><td>Telephone No</td><td>'.$phone_number.'</td></tr><tr><td>Company Category *
+(Please Tick relevant)</td><td>'.$phone_number.'</td></tr></table>
 <br /><br />
 
 <label>This is to confirm our participation in CIO HONOUR AWARDS 2014, and our registration into <strong>'.$cat_name.'</strong> category/ categories. Details of product/ service/ solution are provided in the ensuing sections. </label>
@@ -88,11 +89,12 @@ h1 { margin-bottom: 0; }z
 
 <div align="center"><h1 align="center">CIO HONOUR AWARDS 2014</h1> <h2 align="center">Registration Form </h2></div><hr><br />
 
-<div><label><strong>3) PRODUCT/ SERVICE/ SOLUTION DETAILS</strong></label></div>
+<div><label><strong>3) PRODUCT/ SERVICE/ SOLUTION DETAILS</strong><br/>(To be completed for each category registered)</label></div>
 <br />
-<table border="1" width="100%" height="100%"><tr><td>ICT Category & Code</td><td>'.$cat_name.'</td></tr><tr><td>Product/ Service/Solution Name</td><td>'.$item_name.'</td></tr><tr><td>Product Description</td><td></td></tr></table>
+<table border="1" width="100%" height="100%"><tr><td>ICT Category & Code<br/>(Please select from the
+ICT Category Table)</td><td>'.$cat_name.'</td></tr><tr><td>Product/ Service/Solution Name</td><td>'.$item_name.'</td></tr><tr><td>Brand Name
+(if different from above)</td><td></td></tr><tr><td>Product Description</td><td></td></tr></table>
 <br /><br />
-
 <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 
 <div align="right">Page 2 of 7</div>
@@ -179,7 +181,67 @@ $html.='
 	<br />&nbsp;&nbsp;&nbsp;4)o. &nbsp;“Official Announcement Date” will be the date on which the Honoured status is announced at the recognition &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; event ceremony. This date (subject to any changes that the Organiser may in its absolute discretion make and &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;notify to You) will be published by the Organiser on its official website address at www.cxohonour.com 
 
 </div>
+<div> 
+1.2 Agreement<br />	<br />&nbsp;&nbsp;&nbsp;
+The Signatory, by signing a copy of these Terms (either in his own capacity or on behalf of a person upon whose
+behalf he is authorized to act), will create an Agreement between You and the Organiser which will come into force
+on the date the Terms are signed and which will continue until it is terminated in accordance with Articles 5.2 or 5.3.
+</div>
+<div align="center"><h1 align="center">CIO HONOUR AWARDS 2014</h1> <h2 align="center">Terms & Conditions </h2></div><hr><br />
+<div><label><strong>Article 2</strong></label></div>
+<br />
+<div><label><strong>The Programme</strong></label></div>
+<br />
+<label><strong>2.1</strong></label><br>
+You acknowledge that the Programme is an innovative proprietary, annual marketing programme operated by the Organiser
+which is open, subject to these Terms to new/ existing Product(s) launched in the Territory<br>
+<label><strong>2.2</strong></label><br>
+The Organiser intends that Product(s) of the type typically sold and available in the Territory be entered by You into the
+Programme without limitation as per its relevant ICT category.<br>
+<label><strong>2.3</strong></label><br>
+You may submit any new Product of Yours that has been made generally available in the Territory for a minimum period of six
+months in the market to participate in the Programme.<br>
+<label><strong>2.4 ICT Categories</strong></label><br>
+There is a listing of ICT categories published on the Organiser’s official website (www.cxohonour.com) and You may select and
+propose the appropriate ICT category, however, Product(s) will be classified by the Organiser at its absolute discretion into ICT
+categories, if your proposed classification is found to be inappropriate. The decision of the Organiser in this regard will be final
+and binding. The Organiser reserves the absolute right to amend, add or withdraw one or more ICT categories, depending,
+amongst other things, on the nature and number of applications received, and to assign the Product(s) to the ICT category it
+deems appropriate.<br>
+<label><strong>2.5 Multiple Entries</strong></label><br>
+You may enter Product(s) in the Programme in different ICT categories. In the case of substantially similar Product(s),or the
+same Product sold in different sizes and/ or combinations, you may enter only one Product in any ICT category in any
+Programme Year. However, so long as the Product is different in some significant manner, you may enter more than one
+Product in the same ICT category. The Organiser will have absolute discretion to accept/ reject the Product(s) into the
+Programme or into any particular ICT category, to assign Product(s) to ICT categories and to determine if Product(s) that You
+submit are sufficiently different to warrant multiple entries in a particular ICT category.<br/><br/><br/>
 
+<div><label><strong>Article 3</strong></label></div>
+<br />
+<div><label><strong>Application to the Programme</strong></label></div>
+<br />
+<label><strong>3.1 Application Entry</strong></label><br>
+The completed Registration Form and full support materials must be sent by you to the Organiser as per instructions published
+on its official website address at www.cxohonour.com. The Organiser will have the right to reject (without giving reasons) any
+Registration Form submitted.<br>
+<label><strong>3.2</strong></label><br>
+You acknowledge that by submitting a completed Registration Form You commit yourself to the whole Programme and in
+particular to the payment of any fees that become due under Article 5.2. For the avoidance of doubt, you agree to pay these
+fees to the Organiser and you cannot withdraw from the Programme at any point in time post submitting the entry.<br>
+<label><strong>3.3</strong></label><br>
+The Organiser agrees that, except as otherwise provided in Article 6.3, all information and documents submitted by You will be
+treated by the Organiser as confidential and will not be disclosed or published without Your consent, except as may be required
+by law or any regulatory authority. This does not include information that is already available on public domain or already
+known to the Organisers or and lawfully acquired from the third party.<br>
+<label><strong>3.4 Procedure to Honour a Product</strong></label><br>
+The procedure to be adopted by the Organiser to Honour a Product is as follows: (subject to any changes that the Organiser
+may in its absolute discretion make and notify to You. ): <br>
+<label><strong>3.4.1 CIO voting</strong></label><br>
+The Product(s) to be conferred the “CIO HONOUR AWARD of the Year” for each ICT category will be determined based on the
+endorsements expressed by those CIOs within the respondent group, who have/ may have purchased/ used one or more of
+the Product(s) in the particular ICT category. The group will be reasonably representative of the population of CIOs (as
+determined by the Organiser) and will consist of statistically an appropriate sample size. The online survey is conducted on the
+organiser’s official website www.cxohonour.com<br/><br/><br/>
 ';
 	
 	
--- sql_config/database/cio_db.php
+++ sql_config/database/cio_db.php
@@ -1,10 +1,12 @@
 <?php 
 //connect to db
-	$con = mysql_connect('localhost','ciochoice', '10gXWOqeaf');
+	//$con = mysql_connect('localhost','ciochoice', '10gXWOqeaf');
+	$con = mysql_connect('localhost','root', '');
 	//$con = mysql_connect('cio-choice-201408141000.ctseggpibot4.ap-southeast-1.rds.amazonaws.com','admin', 'H3ll0$c1o7#');
 	
 	//$db	 = mysql_select_db('cio-choice_27march');
 	 $db	 = mysql_select_db('cio_dev');
 	// $web_url ="http://staging.cio-choice.sg";
-	$web_url ="http://cio.cxohonour.com/dev/";
+	//$web_url ="http://cio.cxohonour.com/dev/";
+	$web_url ="http://localhost:8012/ciosg/";
 ?>
